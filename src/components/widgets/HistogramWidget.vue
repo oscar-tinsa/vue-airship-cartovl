4e2d8312c04272aa0ee55f715fd54b3b3820d798
oscar-tinsa <oscar.carbajo@externos.tinsa.com>
2019-02-25 10:19:36 +0100

    Clean code

--- src/components/widgets/HistogramWidget.vue
+++ src/components/widgets/HistogramWidget.vue
@@ -12,94 +12,93 @@
 </style>
 
 <script>
-  /* Amcharts */
-  import * as am4core from "@amcharts/amcharts4/core";
-  import * as am4charts from "@amcharts/amcharts4/charts";
-  import am4themes_animated from "@amcharts/amcharts4/themes/animated";
-  am4core.useTheme(am4themes_animated);
+/* Amcharts */
+import * as am4core from '@amcharts/amcharts4/core';
+import * as am4charts from '@amcharts/amcharts4/charts';
+import am4themes_animated from '@amcharts/amcharts4/themes/animated';
+am4core.useTheme(am4themes_animated);
 
-  export default {
-    name: 'HistogramWidget',
-    props: {
-      title: String,
-      unit: String,
-      formatter: Intl.NumberFormat,
-      valueM: Number,
-      valueW: Number,
-      error: String
+export default {
+  name: 'HistogramWidget',
+  props: {
+    title: String,
+    unit: String,
+    formatter: Intl.NumberFormat,
+    valueM: Number,
+    valueW: Number,
+    error: String
+  },
+  computed: {
+    valueFormattedM: function () {
+      return this.formatter.format(this.valueM)
     },
-    computed: {
-      valueFormattedM: function () {
-        return this.formatter.format(this.valueM)
-      },
-      valueFormattedW: function () {
-        return this.formatter.format(this.valueW)
-      },
-      isLoading: function () {
-        return !this.value
-      }
+    valueFormattedW: function () {
+      return this.formatter.format(this.valueW)
     },
-    mounted() {
-      let chart = am4core.create(this.$refs.chartdiv, am4charts.PieChart);
-      
-      // Add and configure Series
-      var pieSeries = chart.series.push(new am4charts.PieSeries());
-      pieSeries.dataFields.value = "people";
-      pieSeries.dataFields.category = "sexo";
+    isLoading: function () {
+      return !this.value
+    }
+  },
+  mounted () {
+    let chart = am4core.create(this.$refs.chartdiv, am4charts.PieChart)
+    
+    // Add and configure Series
+    var pieSeries = chart.series.push(new am4charts.PieSeries())
+    pieSeries.dataFields.value = 'people'
+    pieSeries.dataFields.category = 'sexo'
 
-      // Let's cut a hole in our Pie chart the size of 30% the radius
-      chart.innerRadius = am4core.percent(30);
+    // Let's cut a hole in our Pie chart the size of 30% the radius
+    chart.innerRadius = am4core.percent(30)
 
-      // Put a thick white border around each Slice
-      pieSeries.slices.template.stroke = am4core.color("#fff");
-      pieSeries.slices.template.strokeWidth = 1;
-      pieSeries.slices.template.strokeOpacity = 1;
-      pieSeries.slices.template
-        // change the cursor on hover to make it apparent the object can be interacted with
-        .cursorOverStyle = [
-          {
-            "property": "cursor",
-            "value": "pointer"
-          }
-        ];
+    // Put a thick white border around each Slice
+    pieSeries.slices.template.stroke = am4core.color('#fff')
+    pieSeries.slices.template.strokeWidth = 1
+    pieSeries.slices.template.strokeOpacity = 1
+    pieSeries.slices.template
+      // change the cursor on hover to make it apparent the object can be interacted with
+      .cursorOverStyle = [
+        {
+          'property': 'cursor',
+          'value': 'pointer'
+        }
+      ]
 
-      pieSeries.alignLabels = false;
-      pieSeries.labels.template.bent = true;
-      pieSeries.labels.template.radius = 3;
-      pieSeries.labels.template.padding(0,0,0,0);
+    pieSeries.alignLabels = false
+    pieSeries.labels.template.bent = true
+    pieSeries.labels.template.radius = 3
+    pieSeries.labels.template.padding(0, 0, 0, 0)
 
-      pieSeries.ticks.template.disabled = true;
+    pieSeries.ticks.template.disabled = true
 
-      // Create a base filter effect (as if it's not there) for the hover to return to
-      var shadow = pieSeries.slices.template.filters.push(new am4core.DropShadowFilter);
-      shadow.opacity = 0;
+    // Create a base filter effect (as if it's not there) for the hover to return to
+    var shadow = pieSeries.slices.template.filters.push(new am4core.DropShadowFilter)
+    shadow.opacity = 0
 
-      // Create hover state
-      var hoverState = pieSeries.slices.template.states.getKey("hover"); // normally we have to create the hover state, in this case it already exists
+    // Create hover state
+    var hoverState = pieSeries.slices.template.states.getKey("hover") // normally we have to create the hover state, in this case it already exists
 
-      // Slightly shift the shadow and make it more prominent on hover
-      var hoverShadow = hoverState.filters.push(new am4core.DropShadowFilter);
-      hoverShadow.opacity = 0.7;
-      hoverShadow.blur = 5;
+    // Slightly shift the shadow and make it more prominent on hover
+    var hoverShadow = hoverState.filters.push(new am4core.DropShadowFilter)
+    hoverShadow.opacity = 0.7
+    hoverShadow.blur = 5
 
-      // Add a legend
-      // chart.legend = new am4charts.Legend();
-      
-      chart.data = [{
-        "sexo": "Hombres",
-        "people": this.valueFormattedM
-      },{
-        "sexo": "Mujeres",
-        "people": this.valueFormattedW
-      }];
+    // Add a legend
+    // chart.legend = new am4charts.Legend()
+    chart.data = [{
+      'sexo': 'Hombres',
+      'people': this.valueFormattedM
+    }, {
+      'sexo': 'Mujeres',
+      'people': this.valueFormattedW
+    }]
 
-      this.chart = chart;
-    },
+    this.chart = chart
+  },
 
-    beforeDestroy() {
-      if (this.chart) {
-        this.chart.dispose();
-      }
+  beforeDestroy () {
+    if (this.chart) {
+      this.chart.dispose()
     }
   }
+}
 </script>
--- src/views/About.vue
+++ src/views/About.vue
@@ -43,12 +43,12 @@
 }
 </style>
 <script>
-import { mapState } from 'vuex';
+import { mapState } from 'vuex'
 /* Amcharts */
-import * as am4core from "@amcharts/amcharts4/core";
-import * as am4charts from "@amcharts/amcharts4/charts";
-import am4themes_animated from "@amcharts/amcharts4/themes/animated";
-am4core.useTheme(am4themes_animated);
+import * as am4core from '@amcharts/amcharts4/core'
+import * as am4charts from '@amcharts/amcharts4/charts'
+import am4ThemesAnimated from '@amcharts/amcharts4/themes/animated'
+am4core.useTheme(am4ThemesAnimated)
 
 export default {
   name: 'About',
@@ -60,61 +60,60 @@ export default {
       'layers'
     ])
   },
-  mounted() {
-    let chart = am4core.create(this.$refs.chartdiv, am4charts.RadarChart);
+  mounted () {
+    let chart = am4core.create(this.$refs.chartdiv, am4charts.RadarChart)
 
-    let data = [];
-    let value1 = 500;
-    let value2 = 600;
+    let data = []
+    let value1 = 500
+    let value2 = 600
 
-    for(var i = 0; i < 12; i++){
-      let date = new Date();
-      date.setMonth(i, 1);
-      value1 -= Math.round((Math.random() < 0.5 ? 1 : -1) * Math.random() * 50);
-      value2 -= Math.round((Math.random() < 0.5 ? 1 : -1) * Math.random() * 50);
-      data.push({date:date, value1:value1, value2:value2})
+    for (var i = 0; i < 12; i++) {
+      let date = new Date()
+      date.setMonth(i, 1)
+      value1 -= Math.round((Math.random() < 0.5 ? 1 : -1) * Math.random() * 50)
+      value2 -= Math.round((Math.random() < 0.5 ? 1 : -1) * Math.random() * 50)
+      data.push({ date: date, value1: value1, value2: value2 })
     }
 
-    chart.data = data;
+    chart.data = data
 
     /* Create axes */
-    let categoryAxis = chart.xAxes.push(new am4charts.DateAxis());
 
-    let valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
-    valueAxis.extraMin = 0.2;
-    valueAxis.extraMax = 0.2;
-    valueAxis.tooltip.disabled = true;
+    let valueAxis = chart.yAxes.push(new am4charts.ValueAxis())
+    valueAxis.extraMin = 0.2
+    valueAxis.extraMax = 0.2
+    valueAxis.tooltip.disabled = true
 
     /* Create and configure series */
-    let series1 = chart.series.push(new am4charts.RadarSeries());
-    series1.dataFields.valueY = "value1";
-    series1.dataFields.dateX = "date";
-    series1.strokeWidth = 3;
-    series1.tooltipText = "{valueY}";
-    series1.name = "Series 2";
-    series1.bullets.create(am4charts.CircleBullet);
+    let series1 = chart.series.push(new am4charts.RadarSeries())
+    series1.dataFields.valueY = 'value1'
+    series1.dataFields.dateX = 'date'
+    series1.strokeWidth = 3
+    series1.tooltipText = '{valueY}'
+    series1.name = 'Series 2'
+    series1.bullets.create(am4charts.CircleBullet)
 
-    let series2 = chart.series.push(new am4charts.RadarSeries());
-    series2.dataFields.valueY = "value2";
-    series2.dataFields.dateX = "date";
-    series2.strokeWidth = 3;
-    series2.tooltipText = "{valueY}";
-    series2.name = "Series 2";
-    series2.bullets.create(am4charts.CircleBullet);
+    let series2 = chart.series.push(new am4charts.RadarSeries())
+    series2.dataFields.valueY = 'value2'
+    series2.dataFields.dateX = 'date'
+    series2.strokeWidth = 3
+    series2.tooltipText = '{valueY}'
+    series2.name = 'Series 2'
+    series2.bullets.create(am4charts.CircleBullet)
 
-    // chart.scrollbarX = new am4core.Scrollbar();
-    // chart.scrollbarY = new am4core.Scrollbar();
+    // chart.scrollbarX = new am4core.Scrollbar()
+    // chart.scrollbarY = new am4core.Scrollbar()
 
-    chart.cursor = new am4charts.RadarCursor();
+    chart.cursor = new am4charts.RadarCursor()
 
-    chart.legend = new am4charts.Legend();
+    chart.legend = new am4charts.Legend()
 
-    this.chart = chart;
+    this.chart = chart
   },
 
-  beforeDestroy() {
+  beforeDestroy () {
     if (this.chart) {
-      this.chart.dispose();
+      this.chart.dispose()
     }
   }
 }
